f925164556c7b7dae56f223383081d27b9189f1f
Hieu Nguyen <demonhunter@gmail.com>
2026-03-21 21:50:49 +0700

    feat:all test_20260321

--- tests/lesson-05/test1.spec.ts
+++ tests/lesson-05/test1.spec.ts
@@ -21,11 +21,11 @@ test("test 01", async ({ page }) => {
     await page
       .locator("//input[@id='profile']")
       .setInputFiles("tests/source/test.txt");
-    //.setInputFiles("C:/Users/PC/OneDrive/Desktop/test.txt");
     await page.locator("//textarea[@id='bio']").fill(`Hieu Nguyen dep zai`);
     await page.locator("//input[@id='rating']").fill("9");
     await page.locator("//label[@class='switch']").click();
     await page.locator("//input[@id='favcolor']").fill("#00ffd5");
+    await page.locator("//input[@id='newsletter']").check();
     const box = await page.locator("//div[@id='starRating']").boundingBox();
     await page.mouse.click(box!.x + box!.width / 2, box!.y + box!.height / 2);
   });
